42aad6382f22ed00f7ddee222fc0c862dd38a24b
user-dev <iretbl@gmail.com>
2026-01-26 10:27:40 +0800

    add congress as apisource providers

--- aiecs/tools/apisource/providers/__init__.py
+++ aiecs/tools/apisource/providers/__init__.py
@@ -9,6 +9,7 @@ from aiecs.tools.apisource.providers.fred import FREDProvider
 from aiecs.tools.apisource.providers.worldbank import WorldBankProvider
 from aiecs.tools.apisource.providers.newsapi import NewsAPIProvider
 from aiecs.tools.apisource.providers.census import CensusProvider
+from aiecs.tools.apisource.providers.congress import CongressProvider
 from aiecs.tools.apisource.providers.alphavantage import AlphaVantageProvider
 from aiecs.tools.apisource.providers.restcountries import RESTCountriesProvider
 from aiecs.tools.apisource.providers.exchangerate import ExchangeRateProvider
@@ -114,6 +115,7 @@ register_provider(FREDProvider)
 register_provider(WorldBankProvider)
 register_provider(NewsAPIProvider)
 register_provider(CensusProvider)
+register_provider(CongressProvider)
 register_provider(AlphaVantageProvider)
 register_provider(RESTCountriesProvider)
 register_provider(ExchangeRateProvider)
@@ -141,6 +143,7 @@ __all__ = [
     "WorldBankProvider",
     "NewsAPIProvider",
     "CensusProvider",
+    "CongressProvider",
     "AlphaVantageProvider",
     "RESTCountriesProvider",
     "ExchangeRateProvider",
